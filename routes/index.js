969a96538f10c183580f103a0017691e3e8c22de
TruongChiBao <chibaotruong1506@gmail.com>
2025-02-20 15:27:33 +0700

    add middleware auth and meRouter

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const authRouter = require("./auth");
 
+
+const auth = require("../middleware/auth");
 const route = (app) => {
+  const meRouter = require("./me")();
   app.use("/auth", authRouter);
+  app.use("/me", auth ,meRouter);
 };
 
 module.exports = route;
