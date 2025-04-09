8577c04d50fa372c0a4c33e8dac0e24a5a35ab97
TruongChiBao <chibaotruong1506@gmail.com>
2025-04-09 23:11:39 +0700

    update model color

--- models/Color.js
+++ models/Color.js
@@ -9,7 +9,7 @@ const colorSchema = new Schema({
 
 colorSchema.statics.checkById = async (colorId, message = "Color") => {
   const isExists = await Color.findById(colorId);
-
+  
   if (!isExists) throw new NotFoundError(message);
 };
 
