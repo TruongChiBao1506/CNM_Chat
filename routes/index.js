154bcd176654f80b8cba4ad03a46a6a94d535e89
TruongChiBao <chibaotruong1506@gmail.com>
2025-04-09 23:09:48 +0700

    update router

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
 const authRouter = require("./auth");
-
-
+const userRouter = require('./user');
+const classifyRouter = require('./classify');
 const auth = require("../middleware/auth");
-const route = (app) => {
-  const meRouter = require("./me")();
+const stickerRouter = require('./sticker');
+
+const route = (app,io) => {
+  const meRouter = require("./me")(io);
+  const conversationRouter = require('./conversation')(io);
+  const friendRouter = require('./friend')(io);
+  const channelRouter = require('./channel')(io);
+  const messageRouter = require('./message')(io);
+  const pinMessageRouter = require('./pinMessage')(io);
+  const voteRouter = require('./vote')(io);
+  app.use("/users", auth, userRouter);
   app.use("/auth", authRouter);
   app.use("/me", auth ,meRouter);
+  app.use('/friends', auth, friendRouter);
+  app.use('/classifies', auth, classifyRouter);
+  app.use('/conversations', auth, conversationRouter);
+  app.use('/channels', auth, channelRouter);
+  app.use('/messages', auth, messageRouter);
+  app.use('/pin-messages', auth, pinMessageRouter);
+  app.use('/votes', auth, voteRouter);
+  app.use('/stickers', auth, stickerRouter);
 };
 
 module.exports = route;
